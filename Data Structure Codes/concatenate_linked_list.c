22b81c6a2b0bc95720c0f9c4bc28483e3f30d597
Aditya Gupta <aditya3492gupta@gmail.com>
2022-12-15 18:53:02 +0530

    Data Structure Problem

--- Data Structure Codes/concatenate_linked_list.c
+++ Data Structure Codes/concatenate_linked_list.c
@@ -1,21 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <conio.h>
 typedef struct node
 {
     int data;
-    struct node *next;
+    struct node *link;
 } Node;
-void display(Node *start)
+Node *getnode()
 {
-    struct node *temp;
+    Node *n1;
+    n1 = (Node *)malloc(sizeof(Node));
+    n1->link = NULL;
+    return n1;
+}
+Node *create()
+{
+    Node *NEW1, *temp, *NEW, *start;
+    int element;
+    char ans = 'y';
+    printf("Enter the element\n");
+    scanf("%d", &element);
+    NEW = getnode();
+    start = NEW;
     temp = start;
+    NEW->data = element;
+    NEW->link = NULL;
+    printf("Do you want to enter more elements:y or n\n");
+    ans = getche();
+    while (ans == 'y')
+    {
+        printf("\nEnter the element\n");
+        scanf("%d", &element);
+        NEW1 = getnode();
+        if (NEW1 == NULL)
+            printf("\nOverflow");
+        else
+        {
+            temp->link = NEW1;
+            NEW1->data = element;
+            temp = NEW1;
+            NEW1->link = NULL;
+        }
+        printf("Do you want to enter more elements:y or n\n");
+        ans = getche();
+    }
+    return start;
+}
+void *display(Node *start)
+{
+    Node *temp;
+    temp = start;
+    printf("\n");
     if (temp == NULL)
         printf("The list is empty \n");
     while (temp != NULL)
     {
         printf("%d->", temp->data);
-        temp = temp->next;
+        temp = temp->link;
     }
     printf("Null\n");
 }
@@ -24,59 +65,30 @@ Node *concatenate(Node *a, Node *b)
 {
     if (a != NULL && b != NULL)
     {
-        if (a->next == NULL)
-            a->next = b;
+        if (a->link == NULL)
+            a->link = b;
         else
-            concatenate(a->next, b);
+            concatenate(a->link, b);
     }
     else
         printf("Either a or b is NULL\n");
     return a;
 }
-Node *getnode()
-{
-    Node *n1;
-    n1 = (Node *)malloc(sizeof(Node));
-    n1->next = NULL;
-    return n1;
-}
+
 int main()
 {
-    Node *prev, *a, *b, *p, *new;
-    int n, i;
-    printf("Enter number of elements in linked list a\n");
-    scanf("%d", &n);
-    a = NULL;
-    printf("Enter the elements in the linked list\n");
-    for (i = 0; i < n; i++)
-    {
-        p = (Node *)malloc(sizeof(Node));
-        scanf("%d", &p->data);
-        p->next = NULL;
-        if (a == NULL)
-            a = p;
-        else
-            prev->next = p;
-        prev = p;
-    }
-    printf("Enter number of elements in linked list b\n");
-    scanf("%d", &n);
-    b = NULL;
-    printf("Enter the elements in the linked list\n");
-    for (i = 0; i < n; i++)
-    {
-        p = (Node *)malloc(sizeof(Node));
-        scanf("%d", &p->data);
-        p->next = NULL;
-        if (b == NULL)
-            b = p;
-        else
-            prev->next = p;
-        prev = p;
-    }
+    Node *start1, *start2, *new;
+    start1 = NULL;
+    start2 = NULL;
+    printf("Enter the elements in the first linked list\n");
+    start1 = create();
+    display(start1);
+    printf("Enter the elements in the second linked list\n");
+    start2 = create();
+    display(start2);
     printf("After concatenation\n");
-    a = concatenate(a, b);
-    display(a);
+    new = concatenate(start1, start2);
+    display(new);
 
     return 0;
 }
